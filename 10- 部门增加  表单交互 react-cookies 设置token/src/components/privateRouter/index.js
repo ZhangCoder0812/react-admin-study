b4502a37b4a4ea035993d92e0a9a80a7a36896bd
ZY <13041895753@163.com>
2020-07-15 09:07:43 +0800

    ''

---
+++ 10- 部门增加  表单交互 react-cookies 设置token/src/components/privateRouter/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import {  Route ,Redirect } from 'react-router-dom'
+import {getToken} from "../../utils/cookies";
+
+const  PrivateRouter =({component:Component,...rest})=> {
+
+       // getToken 获取token 判断是否登录
+
+    return (
+           <Route {...rest} render={routeProps=>(
+
+               getToken() ? <Component {...routeProps} /> :  <Redirect to='/'/>
+           )} />
+       )
+}
+
+export default PrivateRouter
+
+
---
+++ 10- 部门增加  表单交互 react-cookies 设置token/src/views/index/components/aside.js
@@ -0,0 +1,21 @@
+import React , {Component,Fragment} from 'react'
+
+
+import AsideMenu from '../../../components/asideMenu'
+
+class Aside extends Component{
+    constructor(props){
+        super(props)
+        this.state={
+
+        }
+    }
+
+    render(){
+        return (
+            <AsideMenu></AsideMenu>
+        )
+    }
+}
+
+export default Aside
